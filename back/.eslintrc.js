a318681c0a749d1115b7e7ed511f517d651100a5
ArendtKacper <k.arendt@gridaly.com>
2022-04-23 10:36:04 +0200

    add eslint

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -1,20 +1,24 @@
-export default {
+module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    indent: ['error', 'tab'],
+    'indent': [
+      'error',
+      2
+    ],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-    'no-console': 0,
-  },
+    'no-console': 0
+  }
 };
